e436af7d70717db9f1a306970feca9fa6b891dc3
wedyza <ggwp.ezpezlmon@mail.ru>
2023-11-21 16:39:48 +0500

    realized drag and drop mechanic

--- Assets/scripts/Product.cs
+++ Assets/scripts/Product.cs
@@ -1,9 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using Interfaces;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Product : MonoBehaviour, IFilling
+public class Product : MonoBehaviour, IFilling, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
+    private RectTransform _rectTransform;
     public bool isAviable;
+
+    void Start()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+    }
+    
+    public void OnDrag(PointerEventData eventData)
+    {
+        _rectTransform.anchoredPosition += eventData.delta;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Begin draggin");
+        var clone = Instantiate(gameObject, gameObject.transform.position, quaternion.identity);
+        clone.transform.SetParent(transform.parent);
+        clone.transform.localScale = gameObject.transform.localScale;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("end draggin");
+        Destroy(gameObject);
+    }
 }
