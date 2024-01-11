0a33345314675a12c07611d246dd9540f3597e7c
wedyza <ggwp.ezpezlmon@mail.ru>
2024-01-11 13:35:14 +0500

    khinkalis are now on plates

--- Assets/scripts/Plate.cs
+++ Assets/scripts/Plate.cs
@@ -6,11 +6,33 @@ using Random = System.Random;
 
 public class Plate : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
-    private int counter = 0;
+    private int _counter = 0;
+    private Vector2 _lastPos;
+    private Vector3 _startPos;
+    private Transform _startParent;
+    private RectTransform _rectTransform;
+    private CanvasGroup _canvasGroup;
+    private GameObject _drag;
+    private Canvas _mainCanvas;
+    private Plate _child;
+    
+    public Plate example;
+
+    void Start()
+    {
+        _mainCanvas = GetComponentInParent<Canvas>();
+    }
+    
+    void Awake()
+    {
+        _drag = GameObject.FindWithTag("drag");
+        _canvasGroup = GetComponent<CanvasGroup>();
+        _rectTransform = GetComponent<RectTransform>();
+    }
     
     public void PlaceOnPlate(Khinkali khinkalina)
     {
-        var pos = counter % 3;
+        var pos = _counter % 3;
 
         pos -= 1;
         
@@ -20,21 +42,35 @@ public class Plate : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHan
         khinkalina.transform.localPosition = new Vector3(-10 * pos, -10 * pos + 10);
         khinkalina.gameObject.SetActive(true);
         khinkalina.GetComponent<CanvasGroup>().blocksRaycasts = false;
-        counter += 1;
+        _counter += 1;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        
+        _rectTransform.anchoredPosition += eventData.delta / _mainCanvas.scaleFactor;
+        _lastPos = _rectTransform.anchoredPosition;
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        throw new System.NotImplementedException();
+        _child = Instantiate(example, transform.position, Quaternion.identity);
+        _child.transform.SetParent(transform.parent);
+        _child.transform.localScale = transform.localScale;
+        _child.transform.SetAsLastSibling();
+        _startPos = _rectTransform.anchoredPosition;
+        _startParent = _rectTransform.parent;
+        gameObject.transform.SetParent(_drag.transform);
+        _canvasGroup.blocksRaycasts = false;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        throw new System.NotImplementedException();
+        _canvasGroup.blocksRaycasts = true;
+        if (_lastPos == _rectTransform.anchoredPosition)
+        {
+            _rectTransform.SetParent(_startParent, false);
+            _rectTransform.anchoredPosition = _startPos;
+            Destroy(_child);
+        }
     }
 }
